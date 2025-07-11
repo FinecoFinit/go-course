f2e61d3a8e6934f8a118f0f9df4635fb3011bfee
FinecoFinit <magickwizard@outlook.com>
2025-07-11 11:46:23 +0500

    Second task done

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ run:
 	go run main.go
 build:
 	go build -o ./bin/convert ./cmd/convert
+	go build -o ./bin/weeklyCalc ./cmd/weeklyCalc
 	chmod +x ./bin/*
 link:
 	go fmt ./...
---
+++ cmd/weeklyCalc/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shopspring/decimal"
+	"strings"
+	"time"
+)
+
+func main() {
+	var sum decimal.Decimal
+	transactions := []map[int]decimal.Decimal{
+		{1: decimal.NewFromFloat(25000.00)},
+		{1: decimal.NewFromFloat(20000.00)},
+		{2: decimal.NewFromFloat(-9800.00)},
+		{3: decimal.NewFromFloat(-1222.22)},
+		{4: decimal.NewFromFloat(-1500.07)},
+		{5: decimal.NewFromFloat(1201.37)},
+		{6: decimal.NewFromFloat(-100.32)},
+		{7: decimal.NewFromFloat(-523.33)},
+	}
+	for _, t := range transactions {
+		for key, val := range t {
+			fmt.Println(time.Weekday(func(val int) int {
+				if val == 7 {
+					return 0
+				}
+				return val
+			}(key)).String())
+			if val.IsNegative() {
+				fmt.Println("Withdrawal:", strings.Trim(val.String(), "-"), "\n")
+			} else {
+				fmt.Println("Inflow:", val, "\n")
+			}
+			sum = decimal.Sum(sum, val)
+		}
+	}
+	fmt.Println("Weekly summary:", sum)
+}
