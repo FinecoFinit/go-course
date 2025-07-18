1b826ecb81fbe91259a209b043a1dba1ad221a48
FinecoFinit <magickwizard@outlook.com>
2025-07-18 16:24:34 +0500

    Better solution

--- internal/utils/weeklyCalc.go
+++ internal/utils/weeklyCalc.go
@@ -9,18 +9,17 @@ import (
 
 func WeeklyCalc() {
 	var sum decimal.Decimal
-	transactions := []map[int]decimal.Decimal{
-		{1: decimal.NewFromFloat(25000.00)},
-		{1: decimal.NewFromFloat(20000.00)},
-		{2: decimal.NewFromFloat(-9800.00)},
-		{3: decimal.NewFromFloat(-1222.22)},
-		{4: decimal.NewFromFloat(-1500.07)},
-		{5: decimal.NewFromFloat(1201.37)},
-		{6: decimal.NewFromFloat(-100.32)},
-		{7: decimal.NewFromFloat(-523.33)},
+	transactions := [][]decimal.Decimal{
+		1: {decimal.NewFromFloat(25000.00), decimal.NewFromFloat(20000.00)},
+		2: {decimal.NewFromFloat(-9800.00)},
+		3: {decimal.NewFromFloat(-1222.22)},
+		4: {decimal.NewFromFloat(-1500.07)},
+		5: {decimal.NewFromFloat(1201.37)},
+		6: {decimal.NewFromFloat(-100.32)},
+		7: {decimal.NewFromFloat(-523.33)},
 	}
-	for _, t := range transactions {
-		for day, val := range t {
+	for day, t := range transactions {
+		for _, val := range t {
 			fmt.Println(time.Weekday(func(day int) int {
 				if day == 7 {
 					return 0
@@ -32,8 +31,8 @@ func WeeklyCalc() {
 			} else {
 				fmt.Println("Inflow:", val, "\n")
 			}
-			sum = decimal.Sum(sum, val)
 		}
+		sum = decimal.Sum(sum, transactions[day]...)
 	}
 	fmt.Println("Weekly summary:", sum)
 }
